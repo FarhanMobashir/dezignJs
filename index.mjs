abc957f811a212044c0ae09d4b4d8759be2dd3ec
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-10-10 17:53:33 +0530

    adds basic functioanlity and error handling

--- index.mjs
+++ index.mjs
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 import { Command } from "commander/esm.mjs";
 import chalk from "chalk";
-import { init } from "./function.mjs";
-import { help } from "./function.mjs";
+import { init } from "./function.js";
 import ora from "ora";
-import { build } from "./function.mjs";
+import { build } from "./function.js";
 
 const program = new Command();
 
@@ -38,7 +37,6 @@ program
   .command("build")
   .description("building files")
   .action(() => {
-    build();
     const spinner = ora("Building your files").start();
 
     setTimeout(() => {
@@ -48,43 +46,18 @@ program
     setTimeout(() => {
       spinner.color = "green";
       spinner.text = "building css";
-    }, 4000);
+    }, 3000);
 
     setTimeout(() => {
       spinner.stop();
 
       console.log(chalk.bold("finished..."));
-    }, 6000);
+    }, 4000);
+
+    setTimeout(() => {
+      build();
+    }, 5000);
   });
-
-// program
-//   .command("print <projectname>")
-//   .description("testing the generation process | make template")
-//   .action((projectName) => {
-//     const spinner = ora("Site creation in progress").start();
-
-//     setTimeout(() => {
-//       spinner.color = "yellow";
-//       spinner.text = "building html";
-//     }, 2000);
-//     setTimeout(() => {
-//       spinner.color = "green";
-//       spinner.text = "building css";
-//     }, 4000);
-
-//     setTimeout(() => {
-//       spinner.stop();
-
-//       console.log(chalk.bold("finished..."));
-//       console.log(
-//         `run ${chalk.greenBright("cd" + " " + projectName)} to get started`
-//       );
-//     }, 6000);
-//   });
 
 program.parse(process.argv);
 const options = program.opts();
-if (options.help) console.log(help());
-if (options.print) {
-  console.log("Hey you get out!");
-}
